ee952d1de33f7dd6e77403d8d4945d51c152aefb
Casey Foster <c@sey.me>
2017-08-01 14:25:34 -0500

    Fix new error handling, 3.1.1

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const {minify} = require('uglify-js');
 
-module.exports = ({file: {buffer}, options}) => ({
-  buffer: Buffer.from(`${minify(buffer.toString(), options).code}\n`)
-});
+module.exports = ({file: {buffer}, options}) => {
+  const {code, error} = minify(buffer.toString(), options);
+  if (error) throw error;
+
+  return {buffer: Buffer.from(`${code}\n`)};
+};
