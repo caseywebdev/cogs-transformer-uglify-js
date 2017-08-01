9ff194f2ee3f1f2e84809b92831154ebfa4e2831
Casey Foster <c@sey.me>
2017-08-01 14:20:11 -0500

    Use uglify-js@3, 3.1.0

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-const _ = require('underscore');
-const uglifyJs = require('uglify-js');
+const {minify} = require('uglify-js');
 
-const DEFAULTS = {
-  fromString: true
-};
-
-module.exports = ({file: {buffer}, options}) => {
-  try {
-    options = _.extend({}, DEFAULTS, options);
-    return {
-      buffer: Buffer.from(
-        `${uglifyJs.minify(buffer.toString(), options).code}\n`
-      )
-    };
-  } catch (er) {
-    throw er instanceof Error ? er : _.extend(new Error(), er);
-  }
-};
+module.exports = ({file: {buffer}, options}) => ({
+  buffer: Buffer.from(`${minify(buffer.toString(), options).code}\n`)
+});
